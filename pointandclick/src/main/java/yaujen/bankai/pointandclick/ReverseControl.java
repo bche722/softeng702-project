d468ea06faf1799a78abcd99bee52a896aea5d50
LucasSherlock <lshe485@aucklanduni.ac.nz>
2019-09-06 14:03:04 +1200

    more testing

--- pointandclick/src/main/java/yaujen/bankai/pointandclick/ReverseControl.java
+++ pointandclick/src/main/java/yaujen/bankai/pointandclick/ReverseControl.java
@@ -5,7 +5,10 @@ import android.hardware.SensorManager;
 
 import java.util.LinkedList;
 
-
+/**
+ * A class to calculate values for the reverse control method, which aims to keep the cursor
+ * stationary in 2D space while the user moves the phone.
+ */
 public class ReverseControl {
 
     private Accelerometer accelerometer;
@@ -20,43 +23,122 @@ public class ReverseControl {
     private double xVelocity;
     private double yVelocity;
 
+    private double xAccel;
+    private double yAccel;
+
+    private boolean UP;
+    private boolean LEFT;
+
+
+    /**
+     * constructor
+     * @param context
+     */
     public ReverseControl (Context context) {
         accelerometer = new Accelerometer(context);
         accelerometer.mSensorManager.registerListener(accelerometer, accelerometer.mSensor, SensorManager.SENSOR_DELAY_NORMAL);
         xVelocity = 0;
         yVelocity = 0;
+        lastX = 0;
+        lastY = 0;
         xVals = new LinkedList<>();
         yVals = new LinkedList<>();
+        UP = true;
+        LEFT = true;
+        xAccel = 0;
+        yAccel = 0;
     }
 
+    /**
+     * the x value to offset the cursor.
+     * @return
+     */
     public double getXOffset() {
+        xAccel = accelerometer.getX();
+        double avg = updateList(xAccel, xVals);
 
-        double avg = updateList(accelerometer.getX(), xVals);
-        double result = 100*(avg - lastX);
+        if (avg > lastX) {
+            if (LEFT = true) {
+                xVelocity = 0;
+            }
+            LEFT = false;
+        } else {
+            if (LEFT = false) {
+                xVelocity = 0;
+            }
+            LEFT = true;
+        }
+
+
         lastX = avg;
 
-        System.out.println("x: "+result);
 
 
-        xVelocity += result*0.02;
+
+
+        xVelocity += accelerometer.getX()*0.02;
+
+        if (xVelocity > 2) {
+            xVelocity = 2;
+        } else if (xVelocity < -2) {
+            xVelocity = -2;
+        }
+
+
 
         return xVelocity*20;
     }
 
+    /**
+     * the y value to offset the cursor
+     * @return
+     */
     public double getYOffset() {
 
-        double avg = updateList(accelerometer.getY(), xVals);
-        double result = 100*(avg - lastY);
-        lastY = avg;
+        yAccel = accelerometer.getY();
+        double avg = updateList(yAccel, xVals);
 
-        System.out.println("y: "+result);
+        if (avg < lastY) {
+            if (UP = true) {
+                yVelocity = 0;
+            }
+            UP = false;
+        } else {
+            if (UP = false) {
+                yVelocity = 0;
+            }
+            UP = true;
+        }
 
 
-        yVelocity += result*0.02;
+        yVelocity += accelerometer.getY()*0.02;
 
-        return yVelocity*20;
+        if (yVelocity > 2) {
+            yVelocity = 2;
+        } else if (yVelocity < -2) {
+            yVelocity = -2;
+        }
+
+
+        print();
+        return -yVelocity*20;
     }
 
+    private void print() {
+
+        System.out.println("Velocity:");
+        System.out.println("x: "+xVelocity+ "   y: "+ -yVelocity);
+        System.out.println("Acceleration:");
+        System.out.println("x: "+xAccel+ "   y: "+ yAccel);
+        System.out.println("\n\n");
+    }
+
+    /**
+     * uses a linked list of past values to smooth out data.
+     * @param val
+     * @param list
+     * @return
+     */
     private double updateList(double val, LinkedList<Double> list) {
         if (list.size()==SIZE) {
             list.pop();
