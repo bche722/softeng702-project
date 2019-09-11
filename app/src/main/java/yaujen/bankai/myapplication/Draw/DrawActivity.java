bbf860ef08321711048e69f6d866410974370d42
Sijie Zhuo <zhuosijie@hotmail.com>
2019-09-11 12:16:51 +1200

    add draw activity to demo activity

--- app/src/main/java/yaujen/bankai/myapplication/Draw/DrawActivity.java
+++ app/src/main/java/yaujen/bankai/myapplication/Draw/DrawActivity.java
@@ -9,13 +9,14 @@ import android.util.DisplayMetrics;
 import yaujen.bankai.myapplication.DemoActivity;
 import yaujen.bankai.myapplication.R;
 import yaujen.bankai.pointandclick.ClickingMethod;
+import yaujen.bankai.pointandclick.MouseActivity;
 import yaujen.bankai.pointandclick.MouseView;
 
 import static yaujen.bankai.myapplication.DemoActivity.KEY_NAME_CLICKING_METHOD;
 import static yaujen.bankai.myapplication.DemoActivity.KEY_NAME_CONTROL_METHOD;
 import static yaujen.bankai.myapplication.DemoActivity.KEY_NAME_TILT_GAIN;
 
-public class DrawActivity extends AppCompatActivity {
+public class DrawActivity extends MouseActivity {
 
     private MouseView mouseView;
     private ConstraintLayout constraintLayout;
@@ -28,15 +29,15 @@ public class DrawActivity extends AppCompatActivity {
     int tiltGain;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
         constraintLayout = findViewById(R.id.layout);
 
-        mouseView = new MouseView(this);
-        mouseView.enableRecalibrationByVolumeUp(true);
-        constraintLayout.addView(mouseView, -1, MouseView.getFullScreenConstraintLayoutParams());
-        mouseView.setClickingTargetView(constraintLayout);
+//        mouseView = new MouseView(this);
+//        mouseView.enableRecalibrationByVolumeUp(true);
+//        constraintLayout.addView(mouseView, -1, MouseView.getFullScreenConstraintLayoutParams());
+//        mouseView.setClickingTargetView(constraintLayout);
 
 
         paintView = (PaintView) findViewById(R.id.paintView);
@@ -50,11 +51,11 @@ public class DrawActivity extends AppCompatActivity {
         clickingMethod = extras.getString(KEY_NAME_CLICKING_METHOD);
         tiltGain = Integer.parseInt(extras.getString(KEY_NAME_TILT_GAIN));
 
-        mouseView.setClickingMethod(ClickingMethod.valueOf(clickingMethod));
-        mouseView.enablePositionControl(controlMethod.equals(DemoActivity.CONTROL_METHODS[0]));
-        mouseView.setPosTiltGain(tiltGain);
-        mouseView.setVelTiltGain(tiltGain);
-        mouseView.enableRecalibrationByVolumeUp(true);
+//        mouseView.setClickingMethod(ClickingMethod.valueOf(clickingMethod));
+//        mouseView.enablePositionControl(controlMethod.equals(DemoActivity.CONTROL_METHODS[0]));
+//        mouseView.setPosTiltGain(tiltGain);
+//        mouseView.setVelTiltGain(tiltGain);
+//        mouseView.enableRecalibrationByVolumeUp(true);
 
     }
 
